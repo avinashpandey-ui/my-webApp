498d2a30821e720cb1ea0430bfa42509243a310c
Avinash Kumar Pandey <avinashme543@gmail.com>
2023-09-23 15:02:07 +0530

    feat: add intial code

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'my-app';
+
+  printColor(color: string) {
+    console.log(color);
+  }
+}
